7b3e1049e27a47bbfdc6457f5ab784e201fee590
jamie.foy <jamie.foy@DVARIOS05.hampdenandco.com>
2025-02-20 16:07:37 +0000

    Login Test Suite

---
+++ Test Suites/Login/TS2 Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2 Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c930ffc1-7af9-43fb-b200-cceb92277212</testSuiteGuid>
+   <testCaseLink>
+      <guid>3b864947-0295-459c-8595-7a5619669855</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Flexcube/Login/TS2 Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
